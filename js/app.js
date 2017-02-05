d6c83b8513eb7a31162806f9083cd357351bf388
rlui001 <rlui001@ucr.edu>
2017-02-04 23:59:48 -0800

    Hard-coded locations for the map.

---
+++ js/app.js
@@ -0,0 +1,47 @@
+var locations = [
+	{
+		name: "Parking Lot 26",
+		lat: 33.9749888,
+		long: -117.3332816
+	},
+	{
+		name: "Spieth Hall",
+		lat: 33.9724408,
+		long: -117.3267614
+	},
+	{
+		name: "Psychology Building",
+		lat: 33.9722429,
+		long: -117.3264968
+	},
+	{
+		name: "The HUB",
+		lat: 33.9744116,
+		long: -117.3279558
+	},
+	{
+		name: "Bell Tower",
+		lat: 33.9736597,
+		long: -117.3277122
+	},
+	{
+		name: "Bourns A125",
+		lat: 33.975592,
+		long: -117.327635
+	},
+	{
+		name: "UCR Recreation Center",
+		lat: 33.9705396,
+		long: -117.328748
+	},
+	{
+		name: "Pentland Hills Residence",
+		lat: 33.9561598,
+		long: -117.325555
+	}
+	// Map area is University of California, Riverside
+	// lat: 33.973804
+	// long: -117.3312105
+
+];
+
