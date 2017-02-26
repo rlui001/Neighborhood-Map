f371637c9cfa009f2a2e3861f1e88b4ce9e68dba
Ronson Lui <rlui001@ucr.edu>
2017-02-25 17:02:46 -0800

    Bounce animation functionality implemented

--- js/app.js
+++ js/app.js
@@ -1,149 +1,189 @@
-// Global variables
-var map;
+'use strict';
 
-// Hard-coded locations
-var locations = [
+var initialLocations = [
 	{
-		name: "Parking Lot 26",
-		lat: 33.9749888,
-		long: -117.3332816
+		name: 'Bracher Park',
+		lat: 37.370,
+		long: -122.002
 	},
 	{
-		name: "Spieth Hall",
-		lat: 33.9724408,
-		long: -117.3267614
+		name: 'Hacker Dojo (old)',
+		lat: 37.402,
+		long: -122.052
 	},
 	{
-		name: "Psychology Building",
-		lat: 33.9722429,
-		long: -117.3264968
+		name: 'Hacker Dojo (current)',
+		lat: 37.381,
+		long: -121.961
 	},
 	{
-		name: "The HUB",
-		lat: 33.9744116,
-		long: -117.3279558
+		name: 'Red Rock Coffee',
+		lat: 37.393,
+		long:-122.081
 	},
 	{
-		name: "Bell Tower",
-		lat: 33.9736597,
-		long: -117.3277122
+		name: 'Com Tam Thanh (Broken Rice)',
+		lat: 37.309,
+		long: -121.934
 	},
 	{
-		name: "Bourns A125",
-		lat: 33.975592,
-		long: -117.327635
+		name: 'House of Falafel',
+		lat: 37.322,
+		long: -122.018
 	},
 	{
-		name: "UCR Recreation Center",
-		lat: 33.9705396,
-		long: -117.328748
+		name: 'The Prolific Oven',
+		lat: 37.394,
+		long: -121.948
 	},
 	{
-		name: "Pentland Hills Residence",
-		lat: 33.9561598,
-		long: -117.325555
+		name: 'Pho Mai #1 Noodle House',
+		lat: 37.415,
+		long: -121.878
+	},
+	{
+		name: 'Alviso Marina County Park',
+		lat: 37.429,
+		long: -121.984
+	},
+	{
+		name: 'Peet\'s Coffee (aka "Church")',
+		lat: 37.307,
+		long: -121.900
 	}
-	// Map area is University of California, Riverside
-	// lat: 33.973804
-	// long: -117.3312105
 
 ];
 
-// Location object constructor
-function Location(data) {
-	var self = this;
+// Declaring global variables now to satisfy strict mode
+var map;
+var clientID;
+var clientSecret;
 
+
+// formatPhone function referenced from
+// http://snipplr.com/view/65672/10-digit-string-to-phone-format/
+
+function formatPhone(phonenum) {
+    var regexObj = /^(?:\+?1[-. ]?)?(?:\(?([0-9]{3})\)?[-. ]?)?([0-9]{3})[-. ]?([0-9]{4})$/;
+    if (regexObj.test(phonenum)) {
+        var parts = phonenum.match(regexObj);
+        var phone = "";
+        if (parts[1]) { phone += "+1 (" + parts[1] + ") "; }
+        phone += parts[2] + "-" + parts[3];
+        return phone;
+    }
+    else {
+        //invalid phone number
+        return phonenum;
+    }
+}
+
+var Location = function(data) {
+	var self = this;
 	this.name = data.name;
 	this.lat = data.lat;
 	this.long = data.long;
+	this.URL = "";
 	this.street = "";
 	this.city = "";
+	this.phone = "";
 
-	// visibility setting
 	this.visible = ko.observable(true);
 
-	// Give each location a marker
-	this.marker = new google.maps.Marker({
-		position: {lat: data.lat, lng: 	data.long},
-		title: data.name
-	});
+	// var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll='+ this.lat + ',' + this.long + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20160118' + '&query=' + this.name;
 
-}
-
-
-function ViewModel() {
-	var self = this;
-
-	// Search filter variable
-	this.filterLocation = ko.observable("");
-
-	// Create the array for the list
-	this.listArray = ko.observableArray([]);
-
-	// Populate the array with the hard-coded locations
-	locations.forEach(function(loc) {
-		self.listArray.push(new Location(loc));
-	});
-
-	// Initialize map area(UCR) using Google Maps
-	map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 15,
-		center: {lat: 33.973804, lng: -117.3312105}
-	});
-
-	//Place markers based on visibility setting
-	placeMarkers = ko.computed(function() {
-		self.listArray().forEach(function(object) {
-			if (object.visible() == true) {
-				object.marker.setMap(map);
-			}
-			else {
-				object.marker.setMap(null);
-			}
-		});
-	});
-	// this.listArray().forEach(function(object) {
-	// 	if (object.visible() == true) {
-	// 		object.marker.setMap(map);
+	// $.getJSON(foursquareURL).done(function(data) {
+	// 	var results = data.response.venues[0];
+	// 	self.URL = results.url;
+	// 	if (typeof self.URL === 'undefined'){
+	// 		self.URL = "";
 	// 	}
-	// 	else {
-	// 		object.marker.setMap(null);
+	// 	self.street = results.location.formattedAddress[0];
+ //     	self.city = results.location.formattedAddress[1];
+ //      	self.phone = results.contact.phone;
+ //      	if (typeof self.phone === 'undefined'){
+	// 		self.phone = "";
+	// 	} else {
+	// 		self.phone = formatPhone(self.phone);
 	// 	}
+	// }).fail(function() {
+	// 	alert("There was an error with the Foursquare API call. Please refresh the page and try again to load Foursquare data.");
 	// });
 
-	// Create a filtered list
-	this.filterLocations = ko.computed(function() {
-		if(!self.filterLocation()) {
-			self.listArray().forEach(function(object) {
-				object.visible(true);
-			});
-			return self.listArray();
-		}
-		else {
-			return ko.utils.arrayFilter(self.listArray(), function(fil) {
-				filter = self.filterLocation().toLowerCase();
-				if (fil.name.toLowerCase().indexOf(filter) !== -1) {
-					fil.visible(true);
-					return fil.name.toLowerCase().indexOf(filter) !== -1;
-				}
-				else {
-					fil.visible(false);
-				}
+	this.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
+        '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
+        '<div class="content">' + self.street + "</div>" +
+        '<div class="content">' + self.city + "</div>" +
+        '<div class="content">' + self.phone + "</div></div>";
 
-			});
-		}
+	this.infoWindow = new google.maps.InfoWindow({content: self.contentString});
+
+	this.marker = new google.maps.Marker({
+			position: new google.maps.LatLng(data.lat, data.long),
+			map: map,
+			title: data.name
 	});
 
+	// this.showMarker = ko.computed(function() {
+	// 	if(this.visible() === true) {
+	// 		this.marker.setMap(map);
+	// 	} else {
+	// 		this.marker.setMap(null);
+	// 	}
+	// 	return true;
+	// }, this);
 
 
+	// this.bounce = function(place) {
+	// 	google.maps.event.trigger(self.marker, 'click');
+	// };
+};
 
+function AppViewModel() {
+	var self = this;
 
+	this.searchTerm = ko.observable("");
 
+	this.locationList = ko.observableArray([]);
+
+	map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 12,
+			center: {lat: 37.370, lng: -122.002}
+	});
+
+	// Foursquare API settings
+	clientID = "V443OTCAQPJLCRY4QWBFYN3ZK5FDKGJOYDHLMI3O342IRVNN";
+	clientSecret = "AK1JHLEG2D2KW14WF5HYVFNTUYFTBXYS4LDUUNRAHPR5URLB";
+
+	initialLocations.forEach(function(locationItem){
+		self.locationList.push( new Location(locationItem));
+	});
+
+	this.filteredList = ko.computed( function() {
+		var filter = self.searchTerm().toLowerCase();
+		if (!filter) {
+			self.locationList().forEach(function(locationItem){
+				locationItem.visible(true);
+			});
+			return self.locationList();
+		} else {
+			return ko.utils.arrayFilter(self.locationList(), function(locationItem) {
+				var string = locationItem.name.toLowerCase();
+				var result = (string.search(filter) >= 0);
+				locationItem.visible(result);
+				return result;
+			});
+		}
+	}, self);
+
+	this.mapElem = document.getElementById('map');
+	this.mapElem.style.height = window.innerHeight - 50;
 }
 
+function startApp() {
+	ko.applyBindings(new AppViewModel());
+}
 
-
-// Allows initialization of app when Google makes callback
-function initApp() {
-	ko.applyBindings(new ViewModel());
+function errorHandling() {
+	alert("Google Maps has failed to load. Please check your internet connection and try again.");
 }
